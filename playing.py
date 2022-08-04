e3a61241fc2d5d2f692282b348d3de19d0e0e4c2
Shaun Norton <58618146+shaunnnorton@users.noreply.github.com>
2022-08-04 10:32:58 -0700

    Maybe maybe maybe

--- playing.py
+++ playing.py
@@ -9,18 +9,20 @@ class EssentialFunctions():
     def calculate_percentage(self):
         original = None
         discounted = None
+        print("Please enter all amounts without the currency symbol.")
         while True:
             try:
                 original = float(input("What was the original price?"))
-                discounted = float(input("What is hte discounted price?"))
+                discounted = float(input("What is the discounted price?"))
             except:
                 print("Im sorry I didnt understand one of your price inputs.")
                 continue
             else:
                 break
-            print("TOOFAR")
+            print("TOOFAR SOMETHING IS BROKEN")
             break
-        print(original+discounted)
+        print("The calculated percentage is.")
+        print((1.0-(discounted//original))*100)
 
 
 test = EssentialFunctions()
