c776594b5c1d78b8bed9bb3d223d0edd0ee19bb2
Shaun Norton <58618146+shaunnnorton@users.noreply.github.com>
2022-08-04 12:55:24 -0700

    More fun goodies

--- playing.py
+++ playing.py
@@ -7,6 +7,7 @@ class EssentialFunctions():
         pass
 
     def calculate_percentage(self):
+        """Calculate the percentage given an origninal price and discounted price"""
         original = None
         discounted = None
         print("Please enter all amounts without the currency symbol.")
@@ -18,11 +19,39 @@ class EssentialFunctions():
                 print("Im sorry I didnt understand one of your price inputs.")
                 continue
             else:
+                if float(0) in (original):
+                    print("ZEROS ARE RUDE DONT USE THEM")
+                    continue
+
                 break
             print("TOOFAR SOMETHING IS BROKEN")
             break
         print("The calculated percentage is.")
         print(f"{(1.0-(discounted/original))*100:.2f}%")
+
+    
+    def calculate_seccond_discount(self):
+        """Calculate an increased percentage given an original price discounted price and the percentage to add
+        E.G. 20% off an item discounted from 15% off"""
+        curr_price = None
+        original_price = None
+        add_percentage = None
+        while True:
+            try: #Collect all inputs as floats
+                current_price = float(input("What is the current price of the item?"))
+                original_price = float(input("What was the original price of the item?"))
+                add_percentage = float(input("What is the additional percentage you would like to add?"))/100
+            except: #Catch any errors arrising from incomplete inputs
+                print("There was an error wiht one of your inputs. Starting from the begining.")
+                continue
+            else:
+                if float(0) in (current_price,original_price,add_percentage):
+                    print("ZEROS ARE RUDE DONT USE THEM")
+                    continue
+                calculated_percentage = 100*(1-(current_price-(add_percentage*current_price))/original_price)
+                print(f"The new calculated percentage is {calculated_percentage:.2f}%")
+                break
+        
 
 
 test = EssentialFunctions()
